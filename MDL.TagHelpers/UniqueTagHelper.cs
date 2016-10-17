cb0ccddc9d9cfda10bd94573689cb06c1bd642c2
John Kane <john@heliumcreative.com.au>
2016-10-17 21:10:36 +0200

    Initial upload to Github
    Initial Upload to Github for a Sample web application and the folder
    structure for the proposed Tag Helper Library

---
+++ MDL.TagHelpers/UniqueTagHelper.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MDL.TagHelpers
+{
+    public abstract class UniqueTagHelper : TagHelper
+    {
+        public string Id { get; private set; }
+
+        public UniqueTagHelper ()
+        {
+            Id = Guid.NewGuid().ToString("N");
+        }
+    }
+}
